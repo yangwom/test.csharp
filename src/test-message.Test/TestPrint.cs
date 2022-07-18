0c8240b9bb830863675b9d4abd999883b2445473
trybe-tech-ops <82593112+trybe-tech-ops@users.noreply.github.com>
2022-07-18 15:05:55 -0300

    Initial commit

---
+++ src/test-message.Test/TestPrint.cs
@@ -0,0 +1,25 @@
+using Xunit;
+using FluentAssertions;
+using System.IO;
+using System;
+
+namespace test_message.Test;
+
+public class TestPrint
+{
+    [Theory]
+    [InlineData()]
+    public void TestPrintMsgSucess(string Send, string Expected)
+    {
+        using (var NewOutput = new StringWriter())
+        {
+            Console.SetOut(NewOutput);
+
+            Message.PrintMsg();
+
+            string result = NewOutput.ToString().Trim();
+
+            result.Should();      
+        }
+    }
+}
